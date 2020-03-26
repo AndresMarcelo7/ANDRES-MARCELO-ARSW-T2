3eef2f495015f25b7d58c0e5eff60a85f6cadd54
Andres Felipe Marcelo <53924254+AndresMarcelo7@users.noreply.github.com>
2020-03-26 11:39:08 -0500

    Añadiendo logica para el cache

--- src/main/java/edu/eci/arsw/covidsearch/CoronavirusStatsCache/CacheCovid.java
+++ src/main/java/edu/eci/arsw/covidsearch/CoronavirusStatsCache/CacheCovid.java
@@ -1,9 +1,12 @@
 package edu.eci.arsw.covidsearch.CoronavirusStatsCache;
 
-
+import edu.eci.arsw.covidsearch.model.CountryStats;
+import edu.eci.arsw.covidsearch.CoronavirusStatsCache.impl.Tuple;
 
 public interface CacheCovid {
-     
+    void saveStatsinCache(String name,CountryStats country);
+
+    Tuple<CountryStats, Long> getStatsStored(String name);
 
 
 }
--- src/main/java/edu/eci/arsw/covidsearch/CoronavirusStatsCache/impl/CacheCovidImpl.java
+++ src/main/java/edu/eci/arsw/covidsearch/CoronavirusStatsCache/impl/CacheCovidImpl.java
@@ -1,11 +1,27 @@
 package edu.eci.arsw.covidsearch.CoronavirusStatsCache.impl;
 
-
 import edu.eci.arsw.covidsearch.CoronavirusStatsCache.CacheCovid;
+import edu.eci.arsw.covidsearch.model.CountryStats;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class CacheCovidImpl implements CacheCovid {
 
+    private ConcurrentHashMap<String,Tuple<CountryStats,Long>> cache;
 
+    public CacheCovidImpl() {
+        this.cache= new ConcurrentHashMap<String,Tuple<CountryStats,Long>>();
+    }
+
+    @Override
+    public void saveStatsinCache(String name, CountryStats country) {
+        cache.put(name,new Tuple<CountryStats,Long>(country,System.nanoTime()));
+    }
+
+    @Override
+    public Tuple<CountryStats, Long> getStatsStored(String name) {
+        return cache.get(name);
+    }
 }
